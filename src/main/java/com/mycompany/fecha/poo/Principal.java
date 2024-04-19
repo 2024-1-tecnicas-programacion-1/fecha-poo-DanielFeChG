ff9eb7dfe213875827d753c699707d076661d461
DanielFeChG <danielfelipechg@gmail.com>
2024-04-18 22:22:25 -0500

    Se añadió el código de la clase fechas y de la clase principal y se documentó según lo solicitado.

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,22 @@
 package com.mycompany.fecha.poo;
+import java.util.Scanner;
 
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Fecha fecha1; // Definición
+        System.out.println("Ingrese el día: ");
+        int dia = sc.nextInt();
+        System.out.println("Ingrese el mes: ");
+        int mes = sc.nextInt();
+        System.out.println("Ingrese el año: ");
+        int annio = sc.nextInt();
+        fecha1 = new Fecha(dia, mes, annio); //Inicialización
+        System.out.println(fecha1.validarFecha()==true?"La fecha ingresada es válida:":"La fecha es inválida.");
+        if (fecha1.validarFecha()==true){
+            System.out.println(fecha1.fechaLarga());
+            //System.out.println(fecha1.fechaCorta());
+        }
+        
+    }
 }
